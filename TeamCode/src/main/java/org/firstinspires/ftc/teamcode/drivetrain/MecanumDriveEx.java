33cbead03ff1559ec91e95161937b3b244e821d4
ShagunRajput1 <28srajput@westfordk12.us>
2025-07-18 13:09:26 -0400

    iffy outtake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain/MecanumDriveEx.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain/MecanumDriveEx.java
@@ -25,7 +25,7 @@ public class MecanumDriveEx {
 
         frontRight.setDirection(DcMotorEx.Direction.REVERSE);
     }
-    public void drive(Gamepad gamepad1, Telemetry telemetry){
+    public void drive(Gamepad gamepad1){
 
         // speed and strafe on left stick; turn on right stick; it got weird sorry
         x = -gamepad1.right_stick_x;
@@ -57,10 +57,13 @@ public class MecanumDriveEx {
         backLeft.setPower(backLeftPower);
         backRight.setPower(backRightPower);
 
+       /**
         telemetry.addData("Front left Power", frontLeft.getPower());
         telemetry.addData("Front right Power", frontRight.getPower());
         telemetry.addData("Back left Power", backLeft.getPower());
         telemetry.addData("Back right Power", backRight.getPower());
         telemetry.update();
+
+        */
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outtake/Arm.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.outtake;
+
+import static android.os.SystemClock.sleep;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Arm {
+    private Servo arm1;
+    private Servo arm2;
+
+    public Arm(HardwareMap hardwareMap) {
+        arm1 = hardwareMap.get(Servo.class, "arm1");
+        arm2 = hardwareMap.get(Servo.class, "arm2");
+
+        arm2.setDirection(Servo.Direction.REVERSE);
+
+        arm1.setPosition(.2072);
+        arm1.setPosition(.2072);
+
+    }
+
+    public void moveArm(Gamepad gamepad2, Telemetry telemetry) {
+        if (gamepad2.a) {
+            if(arm1.getPosition() == .2072) {
+                arm1.setPosition(.8748);
+                arm2.setPosition(.8748);
+            }
+            else{
+                arm1.setPosition(.2072);
+                arm2.setPosition(.2072);
+            }
+        }
+
+        sleep(50);
+
+        telemetry.addData("ARM1 POSITION: ", arm1.getPosition());
+        telemetry.addData("ARM2 POSITION: ", arm2.getPosition());
+        telemetry.update();
+    }
+}
